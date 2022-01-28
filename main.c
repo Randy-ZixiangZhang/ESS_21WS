2ea2544fb8591aa899f65624cedc8269f65f130f
randy <zixiang.zhang@tum.de>
2022-01-28 16:53:03 +0100

    first stag, without measuring time

--- main.c
+++ main.c
@@ -1,4 +1,3 @@
-
 #include "KeyboardHID.h"
 #include "german_keyboardCodes.h"
 #include "character_define.h"
@@ -43,27 +42,54 @@ void SystemCoreClockSetup(void);
 
 /*
 My Code here
-
 */
+
+/*System Clock*/
+uint32_t SysTickCounter = 0; //32bit for maximum
+uint32_t SysTickOld = 0;
+uint32_t SysTickNew = 0;
+
+void initSysTick() {
+	SysTick_Config(SystemCoreClock/1000);			
+}	
+
+extern void SysTick_Handler(){
+	SysTickCounter++;
+}
+
+
+
+
 uint8_t password_GetNext(void);
+int max_index(uint8_t,int);
 //Global Variable
 
 Character G_Password_Arr[84];//10~20 length of pass, last one enter
-
-uint8_t G_PASSWORD[20];
-unsigned char G_PASS_MODIFIER[20];
-
 uint8_t G_numCharacter;
 
+//Control Flag
 bool G_isLastPassDone = 1;
 bool G_shouldSend = 1;
+int G_indStage = 0;
+int G_indSubStage = 0;
+#define numTotalSubStage 84
+
+enum STATE {Idle,Sending,Create,Success,Summarize};
+enum STATE G_CALLBACK_STATE = Create;
+
 
 //LEDSTATE
 bool G_isNUMon;
 bool G_isCAPon;
 bool G_hasNumOff = false;
 
+//Time Measurement
+uint32_t G_time_1substage[numTotalSubStage];
+int G_IndrightCharac;
+
+//temporary
 uint8_t G_FailTime = 0;
+
 
 /**
  * Main program entry point. This routine configures the hardware required by
@@ -80,7 +106,6 @@ int main(void) {
 		; 
 
 	while (1) {
-
 
 		HID_Device_USBTask(&Keyboard_HID_Interface);
 	}
@@ -103,21 +128,50 @@ bool CALLBACK_HID_Device_CreateHIDReport(
 	/*Control Logic give feedback to main loop
 	wait for callback to finish sending or keep going
 	*/
-		//the password is wrong
-	if(G_hasNumOff  & (G_FailTime < 5)){
+	
+	//Determine which state
+
+
+
+
+
+	//this if only enters if LED off signal is received
+	if(G_hasNumOff  & (G_FailTime < numTotalSubStage)){
 		G_isLastPassDone = true;
 		G_shouldSend = true;
 		G_FailTime++;
 		G_hasNumOff = false;
+		SysTickNew = SysTickCounter;
+		G_time_1substage[G_indSubStage-1] = SysTickNew - SysTickOld;
+
+		//Summarize the substage
+		// if (G_FailTime == (numTotalSubStage - 1)){
+		// 	G_IndrightCharac = max_index(G_time_1substage,numTotalSubStage);
+		// }
+
 	}
+
 	//Create new password
 	if(G_isLastPassDone){
-			G_numCharacter = 84;
-			for(int i=0;i<G_numCharacter;i++){
-				G_Password_Arr[i] = G_CHARAC_ARR[i];
-			}
 
-			G_isLastPassDone = false;
+		//first stage, two characters
+		if(G_indStage == 0){
+			G_numCharacter = 2;
+		}
+		else if(G_indStage > 0){
+			G_numCharacter = G_indStage;
+		}
+		
+		if(G_indSubStage < numTotalSubStage){
+			for(int i=0;i<G_numCharacter;i++){
+				G_Password_Arr[i] = G_CHARAC_ARR[G_indSubStage];
+				}
+			G_Password_Arr[G_numCharacter] = G_ENTER;
+			G_indSubStage++;
+		}
+
+		SysTickOld = SysTickCounter;
+		G_isLastPassDone = false;
 	}
 
 
@@ -125,7 +179,7 @@ bool CALLBACK_HID_Device_CreateHIDReport(
 				   indexToSend = 0;
 
 	if(G_shouldSend){
-		if(indexToSend < G_numCharacter) {
+		if(indexToSend < (G_numCharacter+1)) {
 			if(characterSent) {
 				report->Modifier = 0; 
 				report->Reserved = 0; 
@@ -137,9 +191,12 @@ bool CALLBACK_HID_Device_CreateHIDReport(
 				report->Reserved = 0; 
 				report->KeyCode[0] = G_Password_Arr[indexToSend].key; 
 				characterSent = 1;
+
+				for(int i = 0; i < 10e5; ++i)
+		        ; 
 			}
 		}
-		else if (indexToSend == G_numCharacter){
+		else if (indexToSend == (G_numCharacter+1)){
 			//now characterSent = 0
 			indexToSend = 0;//reset of index
 			G_shouldSend = 0;//so callback stops sending
@@ -151,6 +208,22 @@ bool CALLBACK_HID_Device_CreateHIDReport(
 
 
 uint8_t password_GetNext(){
+
+
+
+int max_index(uint32_t *a, int n)
+{
+	if(n <= 0) return -1;
+	int i, max_i = 0;
+	uint32_t max = a[0];
+	for(i = 1; i < n; ++i){
+		if(a[i] > max){
+			max = a[i];
+			max_i = i;
+		}
+	}
+	return max_i;
+}
 
 //	return password;
 }
